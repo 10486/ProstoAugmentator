52da29353affce1a9e552df19597480a3210d3c4
Vladislav <v.mezhevov@napoleonit.ru>
2021-10-27 12:46:30 +0500

    fix 1

--- set_config.py
+++ set_config.py
@@ -2,4 +2,4 @@ from pathlib import Path
 import shutil
 import os
 path = Path(input("Путь до config файла: "))
-shutil.copy(path, Path(os.path.dirname(os.path.abspath(__file__)))/"config"/"translator.yaml")
+shutil.move(path, Path(os.path.dirname(os.path.abspath(__file__)))/"config"/"translator.yaml")
