ace1820632bd06f12a4e3540e3a343d77ab7f5e4
Vladislav <v.mezhevov@napoleonit.ru>
2021-10-27 10:01:14 +0500

    add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-
+with open("requirements.txt","r") as f:
+    requirements = f.readlines()
 setup(
     name='customaugmentator',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
+    requirements=requirements,
 )
